c030ce926eb085d364ef766dfc1873f2802c8782
dancsbertalan <dancsberc@gmail.com>
2021-11-10 17:12:08 +0100

    initial commit without content

---
+++ App/OTPFlickrSearcher/app/src/main/java/com/bertalandancs/otpflickrsearcher/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.bertalandancs.otpflickrsearcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bertalandancs.otpflickrsearcher.ui.main.SearchFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, SearchFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ App/OTPFlickrSearcher/app/src/main/java/com/bertalandancs/otpflickrsearcher/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.bertalandancs.otpflickrsearcher.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
